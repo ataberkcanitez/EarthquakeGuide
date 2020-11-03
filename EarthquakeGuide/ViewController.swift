b0bb3b828ac6807f575684cd714765cfeff2a90a
Ataberk Canıtez <aticanitez@hotmail.com>
2020-11-03 18:50:39 +0300

    first commit with xcode project

---
+++ EarthquakeGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthquakeGuide
+//
+//  Created by Ataberk Canıtez on 3.11.2020.
+//  Copyright © 2020 Ataberk Canıtez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
